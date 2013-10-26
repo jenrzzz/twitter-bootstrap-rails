c78b05df626b8484204ee0d55cdc714ced9020ef
Jenner LaFave <jenner@jfave.com>
2013-10-25 18:34:26 -0700

    update flash helper to work with v3 alerts better

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -1,20 +1,39 @@
 module BootstrapFlashHelper
-  ALERT_TYPES = [:error, :info, :success, :warning]
+  ALERT_TYPES = [:danger, :error, :alert, :info, :success, :warning, :notice]
+
+  def flash_messages?
+    ALERT_TYPES.any? {|type| flash.key?(type) }
+  end
 
   def bootstrap_flash
     flash_messages = []
+
+    # Hack to make this play nice with Devise controllers
+    if defined?(Devise) && defined?(resource) && !resource.nil? && resource.try(:errors).try(:to_a).try(:any?)
+      flash[:alert] ||= []
+     flash[:alert].concat resource.errors.full_messages
+    end
+
     flash.each do |type, message|
       # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
       next if message.blank?
-      
-      type = :success if type == :notice
-      type = :error   if type == :alert
-      next unless ALERT_TYPES.include?(type)
+
+      type = :info if type == :notice
+      type = :danger  if [:alert, :error].include? type
+      if not ALERT_TYPES.include?(type)
+        Rails.logger.debug "Unknown Bootstrap alert type #{type}. Not rendering flash message #{message}. Edit bootstrap_flash_helper.rb to change this behavior."
+      end
 
       Array(message).each do |msg|
+        if msg.is_a? Array
+          msg = msg.flatten.join ', '
+        elsif !msg.is_a? String
+          msg = msg.to_s
+        end
+
         text = content_tag(:div,
-                           content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
-                           msg.html_safe, :class => "alert fade in alert-#{type}")
+                           content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert", 'aria-hidden' => true) +
+                           msg.html_safe, :class => "alert alert-#{type} alert-dismissable fade in")
         flash_messages << text if msg
       end
     end
